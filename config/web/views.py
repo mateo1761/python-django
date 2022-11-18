2212ce613842fa910919b54ad09d40f1ff8247d4
mateo1761 <mateoquinterozapata@gmail.com>
2022-11-18 18:28:41 -0500

    Se tiene que revisar la funcion objects

--- config/web/views.py
+++ config/web/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from web.formularios.formularioPlatos import FormularioPlatos
 from web.formularios.formularioEmpleados import FormularioEmpleados
 
+from web.models import Platos,Empleados
+
 # Create your views here.
 
 #TODAS LAS VISTAS SON FUNCIONES DE PYTHON
@@ -11,6 +13,11 @@ def Home(request):
     return render(request, 'home.html')
 
 def Platos(request):
+    # Rutina para consulta de datos
+    platosConsultados=Platos.objects.all() #El metodo objects no lo esta recocnociendo
+    # print (vars(platosConsultados=Platos.objects.all()))
+    print(platosConsultados)
+
     # Esta vista va a utilizar un formulario de django
     # Debo crear entonces un objeto de la clase FormularioPlatos
 
@@ -19,8 +26,37 @@ def Platos(request):
     # Creamos un diccionario para enviar el formulario al HTML(TEMPLATE)
     
     data = {
-        'formulario': formulario
+        'formulario': formulario,
+        'bandera':False,
+        'platos':platosConsultados
     }
+
+    #RECIBIMOS LOS DATOS DEL FORMULARIO 
+
+    if request.method == 'POST':
+        datosForms = FormularioPlatos(request.POST)
+        if datosForms.is_valid():
+            datosLimpios = datosForms.cleaned_data
+            print(datosLimpios)
+            #Construir un diccionario de envio de datos gacia la BD
+
+            platoNuevo = Platos(
+                nombre_plato = datosLimpios['nombre'],
+                fotografia = datosLimpios['fotografia'],
+                precio = datosLimpios['precio'],
+                tipo = datosLimpios['tipo'],
+                descripcion = datosLimpios['descripcion']
+            )
+            #Intentare llevar mis datos a la BD
+
+            try:
+                platoNuevo.save()
+                data['bandera']=True
+                print('Exito guardando datos')
+            except Exception as error:
+                print('Error al guardar los datos: ', error)
+                data['bandera']=False
+
 
     return render(request, 'menuPlatos.html', data)
 
